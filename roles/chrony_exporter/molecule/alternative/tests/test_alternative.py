2f1e8a4be8c69502fdd25751dad33b8c2ed47e09
SuperQ <superq@gmail.com>
2023-07-14 09:35:05 +0200

    feat: Add chrony_exporter role
    Add role for deploying the chrony_exporter.
    
    Signed-off-by: SuperQ <superq@gmail.com>

---
+++ roles/chrony_exporter/molecule/alternative/tests/test_alternative.py
@@ -0,0 +1,38 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_directories(host):
+    dirs = [
+        "/etc/chrony_exporter"
+    ]
+    for dir in dirs:
+        d = host.file(dir)
+        assert d.is_directory
+        assert d.exists
+
+
+def test_service(host):
+    s = host.service("chrony_exporter")
+    assert s.is_running
+
+
+def test_protecthome_property(host):
+    s = host.service("chrony_exporter")
+    p = s.systemd_properties
+    assert p.get("ProtectHome") == "yes"
+
+
+def test_socket(host):
+    sockets = [
+        "tcp://127.0.0.1:8080"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
