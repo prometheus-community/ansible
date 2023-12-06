5b732c607dba06e259b5ecc1a03a1c0f6ce20a1f
SuperQ <superq@gmail.com>
2023-12-06 11:17:14 +0100

    feat: Add mecached_exporter role
    Add new role: `memcached_exporter`
    
    Signed-off-by: SuperQ <superq@gmail.com>

---
+++ roles/memcached_exporter/molecule/default/tests/test_default.py
@@ -0,0 +1,64 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_directories(host):
+    dirs = [
+        "/etc/memcached_exporter"
+    ]
+    for dir in dirs:
+        d = host.file(dir)
+        assert d.is_directory
+        assert d.exists
+
+
+def test_files(host):
+    files = [
+        "/etc/systemd/system/memcached_exporter.service",
+        "/usr/local/bin/memcached_exporter"
+    ]
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
+
+
+def test_user(host):
+    assert host.group("memcached-exp").exists
+    assert "memcached-exp" in host.user("memcached-exp").groups
+    assert host.user("memcached-exp").shell == "/usr/sbin/nologin"
+    assert host.user("memcached-exp").home == "/"
+
+
+def test_service(host):
+    s = host.service("memcached_exporter")
+    try:
+        assert s.is_running
+    except AssertionError:
+        # Capture service logs
+        journal_output = host.run('journalctl -u memcached_exporter --since "1 hour ago"')
+        print("\n==== journalctl -u memcached_exporter Output ====\n")
+        print(journal_output)
+        print("\n============================================\n")
+        raise  # Re-raise the original assertion error
+
+
+def test_protecthome_property(host):
+    s = host.service("memcached_exporter")
+    p = s.systemd_properties
+    assert p.get("ProtectHome") == "yes"
+
+
+def test_socket(host):
+    sockets = [
+        "tcp://127.0.0.1:9150"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
