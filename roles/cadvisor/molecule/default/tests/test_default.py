9e8cdccba6a6a5cec2c716b452874bb1ef330ef3
SuperQ <superq@gmail.com>
2024-01-07 15:00:38 +0100

    feat: Add cadvisor role
    Add a role to deploy https://github.com/google/cadvisor.
    
    Signed-off-by: SuperQ <superq@gmail.com>

---
+++ roles/cadvisor/molecule/default/tests/test_default.py
@@ -0,0 +1,69 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_files(host):
+    files = [
+        "/etc/systemd/system/cadvisor.service",
+        "/usr/local/bin/cadvisor"
+    ]
+    for file in files:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_file
+
+
+def test_permissions_didnt_change(host):
+    dirs = [
+        "/etc",
+        "/root",
+        "/usr",
+        "/var"
+    ]
+    for file in dirs:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_directory
+        assert f.user == "root"
+        assert f.group == "root"
+
+
+def test_user(host):
+    assert host.group("root").exists
+    assert "root" in host.user("root").groups
+    assert host.user("root").shell == "/bin/bash"
+    assert host.user("root").home == "/root"
+
+
+def test_service(host):
+    s = host.service("cadvisor")
+    try:
+        assert s.is_running
+    except AssertionError:
+        # Capture service logs
+        journal_output = host.run('journalctl -u cadvisor --since "1 hour ago"')
+        print("\n==== journalctl -u cadvisor Output ====\n")
+        print(journal_output)
+        print("\n============================================\n")
+        raise  # Re-raise the original assertion error
+
+
+def test_protecthome_property(host):
+    s = host.service("cadvisor")
+    p = s.systemd_properties
+    assert p.get("ProtectHome") == "yes"
+
+
+def test_socket(host):
+    sockets = [
+        "tcp://127.0.0.1:8080"
+    ]
+    for socket in sockets:
+        s = host.socket(socket)
+        assert s.is_listening
