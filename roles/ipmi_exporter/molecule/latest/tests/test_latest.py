b3582f1ff90dab13ae8070a047bea13cf8c91c00
SuperQ <superq@gmail.com>
2024-01-07 10:08:14 +0100

    feat: Add ipmi_exporter role
    Add a new role for the `ipmi_exporter`.
    
    Signed-off-by: SuperQ <superq@gmail.com>

---
+++ roles/ipmi_exporter/molecule/latest/tests/test_latest.py
@@ -0,0 +1,43 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import testinfra.utils.ansible_runner
+import pytest
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/systemd/system/ipmi_exporter.service",
+    "/usr/local/bin/ipmi_exporter"
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
+
+
+def test_service(host):
+    s = host.service("ipmi_exporter")
+    try:
+        assert s.is_running
+    except AssertionError:
+        # Capture service logs
+        journal_output = host.run('journalctl -u ipmi_exporter --since "1 hour ago"')
+        print("\n==== journalctl -u ipmi_exporter Output ====\n")
+        print(journal_output)
+        print("\n============================================\n")
+        raise  # Re-raise the original assertion error
+
+
+def test_protecthome_property(host):
+    s = host.service("ipmi_exporter")
+    p = s.systemd_properties
+    assert p.get("ProtectHome") == "yes"
+
+
+def test_socket(host):
+    s = host.socket("tcp://0.0.0.0:9290")
+    assert s.is_listening
