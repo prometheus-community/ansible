8b1b6e36e2e65fd4d03b99cc1e5484318f416bb6
gardar <gardar@users.noreply.github.com>
2024-02-25 03:42:29 +0000

    minor: add protecthome
    Signed-off-by: gardar <gardar@users.noreply.github.com>

--- roles/process_exporter/molecule/default/tests/test_default.py
+++ roles/process_exporter/molecule/default/tests/test_default.py
@@ -1,3 +1,6 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 import os
 import testinfra.utils.ansible_runner
 
@@ -26,6 +29,21 @@ def test_files(host):
         assert f.is_file
 
 
+def test_permissions_didnt_change(host):
+    dirs = [
+        "/etc",
+        "/root",
+        "/usr",
+        "/var"
+    ]
+    for file in dirs:
+        f = host.file(file)
+        assert f.exists
+        assert f.is_directory
+        assert f.user == "root"
+        assert f.group == "root"
+
+
 def test_user(host):
     assert host.group("process-exp").exists
     assert "process-exp" in host.user("process-exp").groups
@@ -35,8 +53,21 @@ def test_user(host):
 
 def test_service(host):
     s = host.service("process_exporter")
-#    assert s.is_enabled
-    assert s.is_running
+    try:
+        assert s.is_running
+    except AssertionError:
+        # Capture service logs
+        journal_output = host.run('journalctl -u process_exporter --since "1 hour ago"')
+        print("\n==== journalctl -u process_exporter Output ====\n")
+        print(journal_output)
+        print("\n============================================\n")
+        raise  # Re-raise the original assertion error
+
+
+def test_protecthome_property(host):
+    s = host.service("process_exporter")
+    p = s.systemd_properties
+    assert p.get("ProtectHome") == "yes"
 
 
 def test_socket(host):
